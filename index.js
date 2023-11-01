31da0daa9ac321e72ce741dc0f3a935cb767df43
Adastephen <adakolestephen@gmail.com>
2023-11-02 00:43:01 +0100

    another commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    document.getElementById('headOne').innerHTML = 'Welcome here';
+}
+function myName() {
+    let firstName = 'Adakole ';
+    let lastName = 'Stehen';
+    let fullName = firstName + lastName;
+    let welcome = 'Welcome ' + fullName;
+    alert(welcome);
+}
+function rite(){
+    document.getElementById('inone').innerHTML ='It is amazing in the following orders';
+}
