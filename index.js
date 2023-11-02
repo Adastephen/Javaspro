0f1079b46747e836eb58a937dcd0637c208a41bd
Adastephen <adakolestephen@gmail.com>
2023-11-02 02:01:24 +0100

    another commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function myFunction() {
     document.getElementById('headOne').innerHTML = 'Welcome here';
 }
+// the name functions in the jojects.
 function myName() {
     let firstName = 'Adakole ';
     let lastName = 'Stehen';
@@ -8,7 +9,11 @@ function myName() {
     let welcome = 'Welcome ' + fullName;
     alert(welcome);
 }
+/* multiline comments in javascrit, this is just like html codes*/
 function rite(){
     document.getElementById('inone').innerHTML ='It is amazing in the following orders';
 }
-console.log(3 + 5);
+console.log(3 + 5);// it is used for debugging
+//document.write is used for testing
+// widow.alert or alert is used to convey a message.
+//innerHTML is used to change content in an element.
